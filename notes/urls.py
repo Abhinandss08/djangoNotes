2e6781454b6d3bddb502255d17da2cf1c4ea5889
Abhinand S S <abhinand@yougotagift.com>
2023-08-31 11:30:29 +0530

    Html files for notes CRUD functionality included

---
+++ notes/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.notes, name="notes"),
+    path('single-note/<str:pk>/', views.single_note, name="single-note"),
+    path('create-note/', views.create_note, name="create-note"),
+    path('update-note/<str:pk>/', views.update_note, name="update-note"),
+    path('delete-note/<str:pk>/', views.delete_note, name="delete-note"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
